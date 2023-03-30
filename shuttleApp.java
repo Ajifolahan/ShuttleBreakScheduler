6ef7dfee90bbe014433bcbdbba522da07488fa58
sspinner224 <sspinner@quinnipiac.edu>
2023-03-30 13:46:28 -0400

    fixed student services

--- shuttleApp.java
+++ shuttleApp.java
@@ -1,9 +1,9 @@
-import java.util.Scanner;
+import java.util.Scanner; 
 
 public class shuttleApp {
     public static void main(String[] args) {
 
-        Scanner input = new Scanner(System.in);
+        Scanner input = new Scanner(System.in);  
 
         //big shuttles
         shuttle Red = new shuttle("Red" , 2);
@@ -15,26 +15,38 @@ public class shuttleApp {
         shuttle Yellow = new shuttle("Yellow", 1);
         shuttle Purple = new shuttle("Purple", 1);
 
-        System.out.println("Welcome to the Quinnipiac Shuttle Helper app! \n Are you a student or a driver? \n Type '1' for student services \n Type '2' for driver services");
+        System.out.println("Welcome to the Quinnipiac Shuttle Helper app! \nAre you a student or a driver? \n Type '1' for student services \n Type '2' for driver services");
         int user = input.nextInt();
 
-        if(user == 1) {
-            System.out.println("Welcome to Shuttle Helper for students \n Please enter your student ID number: ");
-            float id = input.nextFloat();
-            System.out.println("Student Verified \n Have you been waiting for the shuttle for longer than 10 minutes?");
-            String waitTime = input.nextLine();
-
-            if(waitTime.equals("yes")) {
-
-            } else if (waitTime.equals("no")) {
-
+            if(user == 1) {
+                System.out.println("Welcome to Shuttle Helper for students \nPlease enter your student ID number: ");
+                float id = input.nextFloat();
+                System.out.println("Student Verified \nHave you been waiting for the shuttle for longer than 10 minutes? \nEnter '1' for YES and '2' for NO.");
+                int waitTime = input.nextInt();
+                
+                if(waitTime == 1) {
+                    System.out.println("This has been reported. \nDrivers have been alerted and a shuttle is on the way.");
+    
+                } else if (waitTime == 2) {
+                    System.out.println("How long have you been waiting? \nEnter the number of minutes: 0 - 9");
+                    int minutes = input.nextInt();
+                    int wait = 10-minutes;
+                    if (minutes<10) {
+                        System.out.println("Please wait " + wait + " more minutes \nIf there is no shuttle please report this again.");
+                    } else if (minutes>10){
+                        System.out.println("This has been reported.\nA shuttle is on its way.");
+                    } 
+                    
+                } else {
+                    System.out.println("Please enter '1' or '2'");
+                   
+                }
+            } else if (user == 2) {
+    
             } else {
-                System.out.println("Please enter 'yes' or 'no'");
-                waitTime = input.nextLine();
+                System.out.println("Please enter '1' or '2'");
             }
-        } else if (user == 2) {
-
-        }
-
+    
+        
     } 
 }
